18918eef9f8b538a81f6dadbbc74226ed665a4f2
chaincpl-2542 <67253657+chaincpl-2542@users.noreply.github.com>
2025-01-17 23:39:04 +0700

    Create py

---
+++ BallTracking.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+
+lower_color = np.array([29, 86, 6])
+upper_color = np.array([64, 255, 255])
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv, lower_color, upper_color)
+
+    kernel = np.ones((5, 5), np.uint8)
+    mask = cv2.erode(mask, kernel, iterations=2)
+    mask = cv2.dilate(mask, kernel, iterations=2)
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contours:
+        if cv2.contourArea(cnt) > 500:
+            ((x, y), radius) = cv2.minEnclosingCircle(cnt)
+            center = (int(x), int(y))
+
+            if radius > 10:
+                cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+    cv2.imshow('Frame', frame)
+    cv2.imshow('Mask', mask)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
