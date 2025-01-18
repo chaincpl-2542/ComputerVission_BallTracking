13cb7cd70ef4d01d7dadbe33adf36fdb3a8e8bed
ChainCPL <Chaincpl2542@gmail.com>
2025-01-18 09:34:52 +0700

    add ball tricking vdo

--- BallTracking.py
+++ BallTracking.py
@@ -1,28 +1,29 @@
 import cv2
 import numpy as np
 
-lower_color = np.array([29, 86, 6])
-upper_color = np.array([64, 255, 255])
+lower_color = np.array([29, 86, 6])  
+upper_color = np.array([64, 255, 255]) 
 
-cap = cv2.VideoCapture(0)
+video_path = "ball_tracking_example.mp4"  
+cap = cv2.VideoCapture(video_path)
+
+
+kernel = np.ones((5, 5), np.uint8)
 
 while True:
     ret, frame = cap.read()
-    if not ret:
-        break
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     mask = cv2.inRange(hsv, lower_color, upper_color)
 
-    kernel = np.ones((5, 5), np.uint8)
     mask = cv2.erode(mask, kernel, iterations=2)
     mask = cv2.dilate(mask, kernel, iterations=2)
 
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for cnt in contours:
-        if cv2.contourArea(cnt) > 500:
+        if cv2.contourArea(cnt) > 500: 
             ((x, y), radius) = cv2.minEnclosingCircle(cnt)
             center = (int(x), int(y))
 
@@ -30,10 +31,11 @@ while True:
                 cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
                 cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
-    cv2.imshow('Frame', frame)
-    cv2.imshow('Mask', mask)
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == 27:  
+        print("ESC pressed. Exiting...")
         break
 
 cap.release()
